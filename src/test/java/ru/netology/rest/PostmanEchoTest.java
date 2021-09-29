8d7f7dca08a29a8bd563619d857ed4b9999f45d1
Svet21 <chuyak.svetlana@gmail.com>
2021-09-29 17:11:36 +0500

    Check different value

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -16,7 +16,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
